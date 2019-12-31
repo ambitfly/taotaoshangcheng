05e181107c9bb02ae83ce70160787b6c720cb071
ambitfly <ambitfly@qq.com>
2019-12-31 18:48:04 +0800

    订单模块

--- taotao_interface/src/main/java/com/taotao/service/order/OrderService.java
+++ taotao_interface/src/main/java/com/taotao/service/order/OrderService.java
@@ -39,10 +39,26 @@ public interface OrderService {
 
     List<Order> findByIds(String[] ids);
 
+    /**
+     * 批量发货
+     * @param orders
+     */
     void deliveryMany(List<Order> orders);
 
     void orderTimeOutLogic();
 
+    /**
+     * 生成用户订单
+     * @param order
+     * @return
+     */
     Map<String,Object> add(Order order);
+
+    /**
+     * 修改订单状态
+     * @param orderId
+     * @param transactionId
+     */
+    void updatePayStatus(String orderId,String transactionId);
 
 }
